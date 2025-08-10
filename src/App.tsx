a1804c2b5f6b2594e0bfdc4e89ec81275787665e
tmoechel-futura <t.moechel@futura-solutions.de>
2025-08-10 08:56:18 +0200

    initial vite tailwind react setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import TestTailwind from './TestTailwind'
+
+function App() {
+
+  return (
+    <TestTailwind />
+  )
+}
+
+export default App
---
+++ src/TestTailwind.tsx
@@ -0,0 +1,7 @@
+export default function TestTailwind() {
+  return (
+    <div className="bg-green-500 text-white p-10 text-3xl">
+      If this is green, Tailwind works!
+    </div>
+  );
+}
