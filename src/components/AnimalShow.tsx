2a1034171a6159712d31ecdfb942a57cbc90c34c
tmoechel-futura <t.moechel@futura-solutions.de>
2025-08-17 09:36:08 +0200

    setting size of images

--- src/components/AnimalShow.tsx
+++ src/components/AnimalShow.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import cat from '../images/cat.svg';
 import dog from '../images/dog.svg';
 import bird from '../images/bird.svg';
 import cow from '../images/cow.svg';
 import gator from '../images/gator.svg';
 import horse from '../images/horse.svg';
+import heart from '../images/heart.svg';
+
+import { useState } from 'react';
 
 interface AnimalShowProps {
     type: string;
@@ -20,13 +22,23 @@ const svgMap: { [key: string]: string } = {
     cow,
     gator,
     horse,
+    heart
 };
 
-export default function AnimalShow({ type, key }: AnimalShowProps) {
+
+
+export default function AnimalShow({ type }: AnimalShowProps) {
+    const [imageClickCount, setImageClickCount] = useState<number>(10);
+
+    const handleImageClick = () => {
+        setImageClickCount(prevCount => prevCount + 1);
+    }
     return (
-        <div className="text-black p-10 text-3xl">
+        <div onClick={handleImageClick} className="text-black p-10 text-3xl">
             AnimalShow Component: {type}
             <img src={svgMap[type]} alt={type} className="w-32 h-32 mx-auto" />
+            <img src={svgMap["heart"]} alt="heart" style={{ width: `${imageClickCount * 5}px`, height: `${imageClickCount * 5}px` }} 
+            className="mx-auto" />
         </div>
     );
 }
