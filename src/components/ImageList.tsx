a407feedbeb5233b1c7abd8e43181f7a87cfdef3
tmoechel-futura <t.moechel@futura-solutions.de>
2025-08-20 20:18:27 +0200

    new components

---
+++ src/components/ImageList.tsx
@@ -0,0 +1,9 @@
+function ImageList() {
+    return (
+        <div>
+            <h2>ImageList Component</h2>
+        </div>
+    );
+}
+
+export default ImageList;
---
+++ src/components/ImageShow.tsx
@@ -0,0 +1,9 @@
+function ImageShow() {
+    return (
+        <div>
+            <h2>ImageShowt Component</h2>
+        </div>
+    );
+}
+
+export default ImageShow;
