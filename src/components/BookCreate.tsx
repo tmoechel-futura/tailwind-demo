1691a739d88fb7c60df1024918f38e7c8da54d6a
tmoechel-futura <t.moechel@futura-solutions.de>
2025-08-31 09:31:13 +0200

    Add Book Create

--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 interface BookCreateProps {
-  // Define your expected props here, for example:
-  // title?: string;
+  title?: string;
 }
 
-export default function BookCreate( props: BookCreateProps) {
-  return (
-    <div>BookCreate</div>
-  )
+interface BookCreateProps {
+    onBookCreate: (data: string) => void;
+}
+
+export default function BookCreate({ onBookCreate }: BookCreateProps) {
+    const [bookTitle, setBookTitle] = useState<string>("");
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        onBookCreate(bookTitle);
+    }
+
+    const bookTitleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setBookTitle(event.target.value);
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit} className="flex gap-2">
+            <input
+                name="query"
+                value={bookTitle} // controlled component 
+                onChange={bookTitleChange}
+                placeholder="Search…"
+                className="border p-2 rounded w-full"
+            />
+            <button type="submit" className="px-4 py-2 rounded bg-blue-600 text-white">
+                Add Book
+            </button>
+        </form>
+    );
 }
