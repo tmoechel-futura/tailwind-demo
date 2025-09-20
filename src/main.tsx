98b3f598ec3c6a61ffe1074f1948d83daab91fba
tmoechel-futura <t.moechel@futura-solutions.de>
2025-09-20 09:09:18 +0200

    add comments

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import './index.css'
 import App from './App.tsx'
 import BooksProvider from './context/BooksProvider.tsx'
 
+// Concepts of this branch
+// - Understand the useCallback hook
+// - Understant that useEffect should contain the fetchBooks function 
+//   as a dependency
+// - Understand the Cleanup function that gets returned from useEffect 
+// - not in this branch, was explained in video
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BooksProvider>
